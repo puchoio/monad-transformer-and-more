887dbc733cff096302591bf11d3b53713fa78735
Miguel Rafael Esteban Martín <miguel.esteban@logicaalternativa.com>
2018-02-14 12:02:13 +0100

    15º Day

--- src/main/scala/monad/futeither/MonadFuturEitherErrorS.scala
+++ src/main/scala/monad/futeither/MonadFuturEitherErrorS.scala
@@ -18,18 +18,52 @@ object MonadFutEitherS {
     type FutEitherError[T] =  Future[Either[Error,T]]    
     
     def apply( implicit ec: ExecutionContext ) = new MonadFutEitherS
+    
+    // MonadFutEitherS()
   
 }
 
 class MonadFutEitherS(implicit ec : ExecutionContext ) extends Monad[Error, FutEitherError] {
   
-  def pure[T]( value : T ) : FutEitherError[T] = ???
+  def pure[T]( value : T ) : FutEitherError[T] = Future{
+    
+      Right( value )
+    }
   
-  def flatMap[A,T]( from : FutEitherError[A], f : (A) => FutEitherError[T] ) : FutEitherError[T] = ???
+  def flatMap[A,T]( from : FutEitherError[A], f : (A) => FutEitherError[T] ) : FutEitherError[T] = {
+    
+      from flatMap {
+        
+          case Right( a ) => f( a )
+          case Left( e) => raiseError( e)
+        
+      } recoverWith errorFromThrowable
+      
+  }
   
-  def raiseError[T] ( error: Error ) : FutEitherError[T] = ???
+  def raiseError[T] ( error: Error ) : FutEitherError[T] = Future {
+    
+      Left( error )
+  }
 
-  def recoverWith[T]( from : FutEitherError[T], f : (Error) => FutEitherError[T] ) : FutEitherError[T] = ???
+  def recoverWith[T]( from : FutEitherError[T], f : (Error) => FutEitherError[T] ) : FutEitherError[T] = {
+    
+    
+      from flatMap {
+        
+          case Left( e ) =>  f( e ) 
+          case Right( t ) => pure( t )
+        
+      } recoverWith errorFromThrowable
+    
+  }
+  
+  
+  def errorFromThrowable[T] : PartialFunction[Throwable, FutEitherError[T]] = {  
+  
+      case t: Throwable => raiseError( new MyError( t.getMessage ) ) 
+    
+  }
  
   
 }
